2e1cb9bace9f61c7e375d294120ffb5090cbb343
Htyg <oleg.kanakaev@gmail.com>
2022-05-20 03:05:45 +0500

    HW9

---
+++ Homework_9/Homework_9/Class.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassInteraction
+{
+    public static class MyExample
+    {
+        public static int[] GetOneTwoDimentionArray(int length, int min = 0, int max = 100)
+        {
+            Random random = new Random();
+            int[] array = new int[length].Select(x => x = random.Next(min, max + 1)).ToArray();
+            return array;
+        }
+
+        public static void PritOneDimentionArray(int Array)
+        {
+            Console.WriteLine(String.Join(' ', Array));
+        }
+
+        public static int[,] GetIntTwoDimentionArray(int raws = 5, int columns = 5, int min = 0, int max = 100)
+        {
+            Random random = new Random();
+            int[,] Array = new int[raws, columns];
+            for (int i = 0; i < raws; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                    Array[i, j] = random.Next(min, max); ;
+            }
+            return Array;
+        }
+        public static double[,] GetDoubleTwoDimentionArray(int raws = 5, int columns = 5, int min = 0, int max = 100, string type = "int")
+        {
+            Random random = new Random();
+            double[,] Array = new double[raws, columns];
+            for (int i = 0; i < raws; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                    Array[i, j] = random.Next(min, max); ;
+            }
+            return Array;
+        }
+
+
+        public static void PrintIntTwoDimentionArray(int[,] Array, int lengthOfArrayElement = 5)
+        {
+            int startDistanceFromTop = Console.CursorTop;
+            for (int i = 0; i <= Array.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < Array.GetLength(1); j++)
+                {
+                    Console.SetCursorPosition(j * lengthOfArrayElement, i + startDistanceFromTop);
+                    Console.Write(Array[i, j] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static int[] SortArray(int[] Array, int startSort, int endSort)
+        {
+            int referencePoint = new Random().Next(startSort, endSort);
+            int slide = endSort;
+            int c = Array[startSort];
+            Array[startSort] = Array[referencePoint];
+            Array[referencePoint] = c;
+            referencePoint = startSort;
+            while (referencePoint < slide)
+            {
+                if (Array[referencePoint + 1] > Array[referencePoint])
+                {
+                    c = Array[slide];
+                    Array[slide] = Array[referencePoint + 1];
+                    Array[referencePoint + 1] = c;
+                    slide--;
+                }
+                else
+                {
+                    c = Array[referencePoint + 1];
+                    Array[referencePoint + 1] = Array[referencePoint];
+                    Array[referencePoint] = c;
+                    referencePoint++;
+                }
+            }
+            if (startSort < referencePoint)
+                Array = SortArray(Array, startSort, referencePoint - 1);
+            if (endSort > referencePoint + 1)
+                Array = SortArray(Array, referencePoint + 1, endSort);
+            return Array;
+        }
+    }
+}
+
---
+++ Homework_9/Homework_9/Program.cs
@@ -0,0 +1,54 @@
+﻿Print("Задача 64: Задайте значения M и N.\nНапишите программу, которая выведет все натуральные числа по убыванию в промежутке от M до N.");
+int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+if (start > end)
+{
+    int c = start;
+    start = end;
+    end = c;
+}
+GetNatural(start, end);
+
+Print("Задача 66: Задайте значения M и N. \nНапишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.");
+Console.WriteLine(GetSum(start, end));
+
+Print("Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. \nДаны два неотрицательных числа m и n.");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(GetAkerman(m, n));
+
+
+int GetAkerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (n != 0 && m == 0)
+        return GetAkerman(n - 1, 1);
+    else
+        return GetAkerman(n - 1, GetAkerman(n, m - 1));
+}
+
+void GetNatural(int start, int end)
+{
+    if (start >= end)
+        Console.WriteLine(start);
+    else
+    {
+        Console.WriteLine(start);
+        GetNatural(++start, end);
+    }
+}
+
+void Print(string text)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(text);
+    Console.ResetColor();
+}
+
+int GetSum(int start, int end)
+{
+    if (start > end)
+        return start;
+    return start + GetSum(++start, end);
+}
